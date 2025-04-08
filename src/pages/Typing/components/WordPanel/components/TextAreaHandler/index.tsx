d2a483aa675ad7eb0810857934a0ad257be5aeaf
liujiazhou <1351034248@qq.com>
2025-04-08 15:57:47 +0800

    输入法切换提示语更新

--- src/pages/Typing/components/WordPanel/components/TextAreaHandler/index.tsx
+++ src/pages/Typing/components/WordPanel/components/TextAreaHandler/index.tsx
@@ -46,7 +46,7 @@ export default function TextAreaHandler({ updateInput }: { updateInput: (updateO
       onInput={onInput}
       onBlur={onBlur}
       onCompositionStart={() => {
-        alert('您正在使用输入法，请关闭输入法。')
+        alert('您正在使用输入法，请关闭输入法切换至英文模式。')
       }}
     ></textarea>
   )
